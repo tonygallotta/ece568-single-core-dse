318fc9c8727df5d9db70f0604b613ce00deb2e56
Tony Gallotta <tgallotta@signal.co>
2016-03-05 21:19:11 -0600

    plotting results

---
+++ plotter.py
@@ -0,0 +1,75 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+
+groups = {}
+with open("step1_results.csv") as csvfile:
+  reader = csv.reader(csvfile, delimiter=',')
+  # skip header row
+  next(reader, None)
+  for row in reader:
+    group = row[0]
+    config = row[1]
+    if group == "mem":
+      mem_group = row[1].split("-")
+      group = mem_group[0] + "-" + mem_group[1]
+      config = config.replace(group + "-", "")
+      if "l2" in group:
+        if "separated" in config:
+          group += "-separated"
+          config = config.replace("separated-", "")
+        else:
+          group = group.replace("dl2", "l2")
+          group += "-unified"
+    if not group in groups:
+      groups[group] = {
+        "x": [],
+        "y": [],
+        "labels": []
+      }
+    groups[group]["x"].append(float(row[2]))
+    groups[group]["y"].append(float(row[3]))
+    groups[group]["labels"].append(config)
+
+for group_key, data in groups.iteritems():
+  if group_key == "base":
+    continue
+  x = data["x"]
+  y = data["y"]
+  labels = data["labels"]
+  labels.insert(0, "base")
+  colors = cm.rainbow(np.linspace(0, 1, len(y)))
+  fig = plt.figure()
+  #fig = plt.figure(num=1, figsize=(13, 13), dpi=80, facecolor='w', edgecolor='k')
+  points = [plt.scatter(groups["base"]["x"][0], groups["base"]["y"][0], color="black", marker="x")]
+  for i in range(0, len(y)):
+    points.append(plt.scatter(x[i], y[i], color=colors[i]))
+  ax = plt.subplot(111)
+  plt.sca
+  ax.set_title(group_key)
+  ax.set_xlabel("IPC")
+  ax.set_ylabel("EDP")
+  ax.ticklabel_format(useOffset=False, style="plain")
+  plt.legend(points,
+           labels,
+           scatterpoints=1,
+           loc="upper left",
+           bbox_to_anchor=(1, 1),
+           fontsize=8)
+  fig.subplots_adjust(right=0.75)
+  plt.savefig(group_key + ".png")
+  plt.close(fig)
+
+
+
+# lo = plt.scatter(random(10), random(10), marker='x', color=colors[0])
+# ll = plt.scatter(random(10), random(10), marker='o', color=colors[0])
+# l  = plt.scatter(random(10), random(10), marker='o', color=colors[1])
+# a  = plt.scatter(random(10), random(10), marker='o', color=colors[2])
+# h  = plt.scatter(random(10), random(10), marker='o', color=colors[3])
+# hh = plt.scatter(random(10), random(10), marker='o', color=colors[4])
+# ho = plt.scatter(random(10), random(10), marker='x', color=colors[4])
+
+
+plt.show()
